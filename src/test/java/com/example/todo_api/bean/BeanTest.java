4ce95d573aea4b2a06d5428904b8a46b65c72742
hyeok <hyeok3767@gmail.com>
2025-04-15 23:15:27 +0900

    week2-1

---
+++ src/test/java/com/example/todo_api/bean/BeanTest.java
@@ -0,0 +1,36 @@
+package com.example.todo_api.bean;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+public class BeanTest {
+
+    ApplicationContext context = new AnnotationConfigApplicationContext(TestConfig.class);
+
+    @Test
+    public void getAllBeanTest() {
+
+        for (String name : context.getBeanDefinitionNames()){
+            System.out.println(name);
+        }
+
+        Assertions.assertThat(context.getBeanDefinitionNames()).contains("myBean");
+    }
+
+    @Test
+    public void getOneBeanTest() {
+        MyBean myBean1 = context.getBean(MyBean.class);
+        MyBean myBean2 = context.getBean(MyBean.class);
+
+        MyBean myBean3 = new MyBean();
+
+        System.out.println(myBean1);
+        System.out.println(myBean2);
+        System.out.println(myBean3);
+
+        Assertions.assertThat(myBean1).isSameAs(myBean2);
+    }
+}
